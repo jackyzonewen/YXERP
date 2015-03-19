2136d50d92a4574e8940b61e04e82e6c8ccbcbe1
sunallen521 <sunallen@163.com>
2015-03-19 21:22:00 +0800

    样式调整

---
+++ YXManage1.0/modules/scripts/layout.js
@@ -0,0 +1,19 @@
+﻿
+//页面加载
+$(document).ready(function () {
+    $(".main-body").css("min-height", document.documentElement.clientHeight - 134);
+    //隐藏下拉菜单
+    $(".nav-class").click(
+        function () {
+            if ($(this).find("img").attr("title") == "展开") {
+                $(this).find("img").attr("src", "/modules/images/pull.png").attr("title", "收起");
+                $(this).next().slideDown("normal");
+            }
+            else {
+                $(this).find("img").attr("src", "/modules/images/open.png").attr("title", "展开");
+                $(this).next().slideUp("normal");
+            }
+        }
+    );
+    
+});
