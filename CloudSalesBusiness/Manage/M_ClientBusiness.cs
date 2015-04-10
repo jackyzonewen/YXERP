927c3af6003a3a1a6683ba11c2a1e278ad5a6e00
sunallen521 <sunallen@163.com>
2015-04-10 17:05:13 +0800

    编辑

--- CloudSalesBusiness/Manage/M_ClientBusiness.cs
+++ CloudSalesBusiness/Manage/M_ClientBusiness.cs
@@ -15,6 +15,13 @@ namespace CloudSalesBusiness
 
         #region 添加
 
+        /// <summary>
+        /// 添加客户端
+        /// </summary>
+        /// <param name="model">M_Clients 对象</param>
+        /// <param name="loginName">登陆账号</param>
+        /// <param name="loginPwd">密码</param>
+        /// <returns></returns>
         public static string InsertClient(M_Clients model, string loginName, string loginPwd)
         {
             string clientid = new M_ClientDAL().InsertClient(model.CompanyName, loginName, loginPwd);
