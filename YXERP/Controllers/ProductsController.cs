36a9ca765b90ff88510affcc429449965eddd583
jianrao <Ice_swordman@163.com>
2015-05-06 21:38:39 +0800

    添加视图

--- YXERP/Controllers/ProductsController.cs
+++ YXERP/Controllers/ProductsController.cs
@@ -16,5 +16,58 @@ namespace YXERP.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 品牌列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Brand() 
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 产品单位列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Unit() 
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 产品分类列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Category() 
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 产品属性
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Attr() 
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 添加产品
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ProductsAdd() 
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 产品列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ProductsList() 
+        {
+            return View();
+        }
     }
 }
