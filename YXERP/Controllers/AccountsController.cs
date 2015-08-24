dce6c6b0a90530a391489d5c484334cc74ccf1dd
sunallen521 <sunallen@163.com>
2015-08-24 22:55:36 +0800

    产品分类样式调整

---
+++ YXERP/Controllers/AccountsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YXERP.Controllers
+{
+    public class AccountsController : Controller
+    {
+        //
+        // GET: /Accounts/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
