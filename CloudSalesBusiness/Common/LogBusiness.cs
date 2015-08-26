47138de8132b475e6624115b6572514880d9e912
sunallen521 <sunallen@163.com>
2015-08-26 23:27:25 +0800

    更换为framework4.5, 登录日志逻辑完成

---
+++ CloudSalesBusiness/Common/LogBusiness.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CloudSalesDAL;
+using System.Threading.Tasks;
+using CloudSalesEnum;
+
+namespace CloudSalesBusiness
+{
+    public class LogBusiness
+    {
+        /// <summary>
+        /// 添加登录日志
+        /// </summary>
+        /// <param name="loginname">用户名</param>
+        /// <param name="status">登录结果</param>
+        /// <param name="logintype">系统类型</param>
+        /// <param name="operateip">登录IP</param>
+        public static async void AddLoginLog(string loginname, bool status, EnumLoginType logintype, string operateip)
+        {
+            await LogDAL.AddLoginLog(loginname, status ? 1 : 0, (int)logintype, operateip);
+        }
+    }
+}
---
+++ CloudSalesDAL/Common/LogDAL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSalesDAL
+{
+    public class LogDAL :BaseDAL
+    {
+        public static Task<bool> AddLoginLog(string loginname, int status, int logintype, string operateip)
+        {
+            string sqlText = "insert into Log_Login(LoginName,LoginType,Status,CreateTime,OperateIP) values(@LoginName,@LoginType,@Status,GETDATE(),@OperateIP)";
+            SqlParameter[] paras = { 
+                                     new SqlParameter("@LoginName" , loginname),
+                                     new SqlParameter("@LoginType" , logintype),
+                                     new SqlParameter("@Status" , status),
+                                     new SqlParameter("@OperateIP" , operateip)
+                                   };
+            return Task.Run(() => { return ExecuteNonQuery(sqlText, paras, CommandType.Text) > 0; });
+        }
+    }
+}
