63621d33cfff6e1c0e8c5b90e254ca628a835737
sunallen521 <sunallen@163.com>
2015-09-16 16:45:22 +0800

    BUG

--- CloudSalesDB/Procedures/Stock/P_InsertProduct.sql
+++ CloudSalesDB/Procedures/Stock/P_InsertProduct.sql
@@ -68,9 +68,9 @@ BEGIN
 		--不存在规格，插入默认子产品
 		if not exists (select AutoID from CategoryAttr where CategoryID=@CategoryID and Type=2 and Status=1)
 		begin
-			INSERT INTO ProductDetail(ProductDetailID,[ProductID],DetailsCode,[UnitID] ,[SaleAttr],[AttrValue],[SaleAttrValue],[Price],[Status],
+			INSERT INTO ProductDetail(ProductDetailID,[ProductID],DetailsCode ,[SaleAttr],[AttrValue],[SaleAttrValue],[Price],[BigPrice],[Status],
 					Weight,ImgS,[ShapeCode] ,[Description],[CreateUserID],[CreateTime] ,[UpdateTime],[OperateIP] ,[ClientID])
-				VALUES(NEWID(),@ProductID,'',@SmallUnitID,'','','',@Price,1,
+				VALUES(NEWID(),@ProductID,'','','','',@Price,@Price*@BigSmallMultiple,1,
 					@Weight,'','','',@CreateUserID,getdate(),getdate(),'',@ClientID);
 			set @Err+=@@Error
 		end
